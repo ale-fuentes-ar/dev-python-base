39a347306a19bcccbaca3d7f0a0961f8b7c0ecc1
ale.fuentes.ar <ale.fuentes.ar@hotmail.com>
2023-10-31 05:55:28 -0300

    feat - new version hello.py

--- hello.py
+++ hello.py
@@ -19,7 +19,7 @@ Execution
 """
 # Dunder: definition var that start with tow underlines
 # and finish with two underline
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Alejandro Fuentes"
 __licence__ = "Unlicense"
 
@@ -28,14 +28,30 @@ import os
 
 # snake case: format of names vars and functions.
 current_language = os.getenv("LANG", "en_US.utf8").split(".")[0]
-msg = "Gretting generic!"
+print(current_language)
+
 
 # I can to configure my LANG, ussing 'export LANG=<idiom>'
 # I can eraser var LANG, ussing 'unset LANG'
+# Time Complexity = O(n)
+msg = "Gretting generic!"
+
 if current_language == "en_US":
     msg = "Hello, World - variavel!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
 
-print(current_language)
 print(msg)
+
+# sets (Hash Table) - O(1) - constant
+# dicts (Has Table)
+msg_dict = {
+    "en_US": "Hello, World!",
+    "es_SP": "Hola, Mundo!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "fr_FR": "Ça va, Monde!",
+}
+
+print(msg_dict[current_language])
+
