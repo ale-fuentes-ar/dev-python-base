708233f2c0b25838ced21ffbb27a19b87124cb36
ale.fuentes.ar <ale.fuentes.ar@hotmail.com>
2023-11-04 16:17:59 -0300

    Errors : use EAFP, try, exept, rice

--- errors.py
+++ errors.py
@@ -5,20 +5,21 @@ import sys
 
 # EAFP - Easy to Ask Forgiveness than Permission
 try:
-    names = open('names.txt').readlines() # FileNotFoundError
-    1 / 1 #  ZeroDivisionError
-    print(names.append) # AttributeError
-# except: # Bare Except
-except FileNotFoundError:
-    print("[ERROR] : File name.txt not found")
-    sys.exit(1)
-except ZeroDivisionError:
-    print("[ERROR] : You cant divide by zero")
-    sys.exit(1)
-except AttributeError:
-    print("[ERROR] : List doesn't have oranges")
-    sys.exit(1)
+    raise RuntimeError("A error ocurred")
+except Exception as e:
+    print(str(e))
 
+
+try:
+    names = open('names.txt').readlines()
+except FileNotFoundError as e:
+    print(f"{str(e)}")
+    sys.exit(1)
+    # TODO: will use retry
+else:
+    print("Sucess !!!")
+finally:
+    print("Execute always...")
 try:
     print(names[2])
 except:
