8af7a193a1b605c88a5f959d15fe5d11aceccbe6
ale.fuentes.ar <ale.fuentes.ar@hotmail.com>
2023-11-04 15:57:43 -0300

    Errors : simulate several eceptions

--- errors.py
+++ errors.py
@@ -3,21 +3,25 @@
 import os
 import sys
 
-# LBYL - Look Before You Leap
-if os.path.exists('names.txt'):
-    # names = ['ale', 'jandro', 'fuentes']
-    input("...") # Race Condition
-    names = open('names.txt').readlines()
-else:
+# EAFP - Easy to Ask Forgiveness than Permission
+try:
+    names = open('names.txt').readlines() # FileNotFoundError
+    1 / 1 #  ZeroDivisionError
+    print(names.append) # AttributeError
+# except: # Bare Except
+except FileNotFoundError:
     print("[ERROR] : File name.txt not found")
     sys.exit(1)
+except ZeroDivisionError:
+    print("[ERROR] : You cant divide by zero")
+    sys.exit(1)
+except AttributeError:
+    print("[ERROR] : List doesn't have oranges")
+    sys.exit(1)
 
-
-if len(names) >= 4:
-    print(names)
-    print(len(names))
-    print(names[3])
-else:
+try:
+    print(names[2])
+except:
     print("[ERROR] : Missing name in the list")
     sys.exit(1)
  
