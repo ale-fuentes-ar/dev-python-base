00906d2dbcbba1d8347946ea2b81547071bd5c0a
ale.fuentes.ar <ale.fuentes.ar@hotmail.com>
2023-11-07 18:36:08 -0300

    feat - add logs

--- logs.py
+++ logs.py
@@ -4,6 +4,7 @@
 
 import os
 import logging
+from logging import handlers
 
 
 # TODO: convert setting logs in a function
@@ -13,16 +14,24 @@ log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 # instance
 log = logging.Logger("ale", log_level)
 # level : dev, tqs, etc
-ch = logging.StreamHandler()
-ch.setLevel(log_level)
+# ch = logging.StreamHandler()
+# ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "logs-file.log", 
+    maxBytes=100, # indicating to use 10**6,
+    backupCount=10
+)
+fh.setLevel(log_level)
 # format: more information
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s '
     'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
+# ch.setFormatter(fmt)
 # target: set target
-log.addHandler(ch)
+# log.addHandler(ch)
+fh.setFormatter(fmt)
+log.addHandler(fh)
 
 """
 # logging : root logger
@@ -37,8 +46,8 @@ print("-" * 10)
 try:
     1/0
 except ZeroDivisionError as e:
-    print(f"[ERRO] has a error {str(e)}")
-    logging.error("Has a error %s", str(e))
+    # print(f"[ERRO] has a error {str(e)}")
+    log.error("Has a error %s", str(e))
     # stdout
     # stderr
     # python logs.py &2> /tmp/error.log
